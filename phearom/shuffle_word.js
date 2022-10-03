470d699c2ad76f3069ef6ecab7f8d2d26fb2fde4
Sean-Sophearom <sean.sophearom77@gmail.com>
2022-10-03 21:05:11 +0700

    add a bit of animation

---
+++ phearom/shuffle_word.js
@@ -0,0 +1,41 @@
+function shuffleWord(domName, wordList) {
+  let myInterval = setInterval(() => displayName(domName, wordList), 75);
+  let i = -1;
+  function displayName(domName, wordList) {
+    wordList = [1, ...wordList];
+    if (i > 0) {
+      let letter = wordList[i][domName.innerText.length];
+      domName.innerText +=
+        letter !== " "
+          ? letter
+          : " " + wordList[i][domName.innerText.length + 1];
+      if (domName.innerText === wordList[i]) {
+        i = -i;
+        clearInterval(myInterval);
+        myInterval = setInterval(
+          () => displayName(domName, wordList.slice(1)),
+          75
+        );
+      }
+    } else {
+      domName.innerText = domName.innerText.slice(0, -1);
+      if (domName.innerText.length == 0) {
+        i = -i === wordList.length - 1 ? 1 : -i + 1;
+        domName.innerText = wordList[i][0];
+        clearInterval(myInterval);
+        myInterval = setInterval(
+          () => displayName(domName, wordList.slice(1, wordList.length)),
+          150
+        );
+      }
+    }
+  }
+}
+
+const domWord = document.getElementById("phearom__job");
+
+shuffleWord(domWord, [
+  "Frontend Developer...",
+  "Web Developer...",
+  "Backend Developer...",
+]);
