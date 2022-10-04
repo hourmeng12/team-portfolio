b4b755edbdba0aacb508e9e488c0d0f2db65512f
Sean-Sophearom <sean.sophearom77@gmail.com>
2022-10-04 15:39:34 +0700

    white icon on darkmode

--- scripts/dark_mode.js
+++ scripts/dark_mode.js
@@ -4,6 +4,9 @@ const prefer_dark_mode = JSON.parse(localStorage.getItem("prefer_dark_mode"));
 if (prefer_dark_mode) {
   document.body.classList.add("dark");
   dark__btn.innerText = "Light";
+  document
+    .querySelectorAll(".icon")
+    .forEach((i) => i.classList.add("white-icons"));
 }
 
 function toggleDarkMode() {
@@ -12,9 +15,15 @@ function toggleDarkMode() {
     body.classList.remove("dark");
     dark__btn.innerText = "Dark";
     localStorage.removeItem("prefer_dark_mode");
+    document
+      .querySelectorAll(".icon")
+      .forEach((i) => i.classList.remove("white-icons"));
   } else {
     body.classList.add("dark");
     dark__btn.innerText = "Light";
     localStorage.setItem("prefer_dark_mode", JSON.stringify(true));
+    document
+      .querySelectorAll(".icon")
+      .forEach((i) => i.classList.add("white-icons"));
   }
 }
