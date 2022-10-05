3d3249bbfc86dd5de42c34da293c669cb761f144
Hourmeng Khy <itshourmeng@gmail.com>
2022-10-05 13:12:40 +0700

    Rename dark-mode class

--- scripts/dark_mode.js
+++ scripts/dark_mode.js
@@ -1,27 +1,27 @@
-const dark__btn = document.getElementById("dark__mode__btn");
-dark__btn.addEventListener("click", toggleDarkMode);
-const prefer_dark_mode = JSON.parse(localStorage.getItem("prefer_dark_mode"));
+const dark__btn = document.getElementById('dark-mode__btn');
+dark__btn.addEventListener('click', toggleDarkMode);
+const prefer_dark_mode = JSON.parse(localStorage.getItem('prefer_dark_mode'));
 if (prefer_dark_mode) {
-  document.body.classList.add("dark");
+  document.body.classList.add('dark');
   dark__btn.checked = true;
   document
-    .querySelectorAll(".icon")
-    .forEach((i) => i.classList.add("white-icons"));
+    .querySelectorAll('.icon')
+    .forEach((i) => i.classList.add('white-icons'));
 }
 
 function toggleDarkMode() {
   const body = document.body;
-  if (body.classList.contains("dark")) {
-    body.classList.remove("dark");
-    localStorage.removeItem("prefer_dark_mode");
+  if (body.classList.contains('dark')) {
+    body.classList.remove('dark');
+    localStorage.removeItem('prefer_dark_mode');
     document
-      .querySelectorAll(".icon")
-      .forEach((i) => i.classList.remove("white-icons"));
+      .querySelectorAll('.icon')
+      .forEach((i) => i.classList.remove('white-icons'));
   } else {
-    body.classList.add("dark");
-    localStorage.setItem("prefer_dark_mode", JSON.stringify(true));
+    body.classList.add('dark');
+    localStorage.setItem('prefer_dark_mode', JSON.stringify(true));
     document
-      .querySelectorAll(".icon")
-      .forEach((i) => i.classList.add("white-icons"));
+      .querySelectorAll('.icon')
+      .forEach((i) => i.classList.add('white-icons'));
   }
 }
