e2883505937ea211f78d21b95fea3320c396ba09
Sean-Sophearom <sean.sophearom77@gmail.com>
2022-10-05 12:27:37 +0700

    change to dark mode icon

--- scripts/dark_mode.js
+++ scripts/dark_mode.js
@@ -3,7 +3,7 @@ dark__btn.addEventListener("click", toggleDarkMode);
 const prefer_dark_mode = JSON.parse(localStorage.getItem("prefer_dark_mode"));
 if (prefer_dark_mode) {
   document.body.classList.add("dark");
-  dark__btn.innerText = "Light";
+  dark__btn.checked = true;
   document
     .querySelectorAll(".icon")
     .forEach((i) => i.classList.add("white-icons"));
@@ -13,14 +13,12 @@ function toggleDarkMode() {
   const body = document.body;
   if (body.classList.contains("dark")) {
     body.classList.remove("dark");
-    dark__btn.innerText = "Dark";
     localStorage.removeItem("prefer_dark_mode");
     document
       .querySelectorAll(".icon")
       .forEach((i) => i.classList.remove("white-icons"));
   } else {
     body.classList.add("dark");
-    dark__btn.innerText = "Light";
     localStorage.setItem("prefer_dark_mode", JSON.stringify(true));
     document
       .querySelectorAll(".icon")
